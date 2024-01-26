9a9e18dd0e9cce537c8f841aeb0e6d6519402b75
hewasajumperboy <brandon@brocker.in>
2024-01-26 06:43:39 -0800

    Welcome back to Well There's Your Problem where prints are mirrored and nobody pays attention to the right hand rule

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1658,7 +1658,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
-#define INVERT_Y_DIR true
+#define INVERT_Y_DIR false
 #define INVERT_Z_DIR false
 //#define INVERT_I_DIR false
 //#define INVERT_J_DIR false
@@ -2152,7 +2152,7 @@
  *    +-------------->X     +-------------->X     +-------------->Y
  *     XY_SKEW_FACTOR        XZ_SKEW_FACTOR        YZ_SKEW_FACTOR
  */
-#define SKEW_CORRECTION
+//#define SKEW_CORRECTION
 
 #if ENABLED(SKEW_CORRECTION)
   // Input all length measurements here:
