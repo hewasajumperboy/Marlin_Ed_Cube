25cf801e0b75fba05fc0928b037ff4341c423862
hewasajumperboy <brandon@brocker.in>
2024-02-11 21:45:33 -0800

    Best guess at bed/printer geometry (Ender3 bed w/ 400mm z-rails)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1812,8 +1812,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 235
+#define Y_BED_SIZE 235
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1821,7 +1821,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 350
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
