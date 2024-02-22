729dc0a1998ebb9ce40717e00c16b3ede9f63de5
hewasajumperboy <brandon@brocker.in>
2024-02-21 19:27:00 -0800

    Increment config string

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -61,7 +61,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Brandon Brocker, 20240211)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Brandon Brocker, 20240221)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 // @section machine
