08921e2ce5382f7e6568cd10a82147c408490211
hewasajumperboy <brandon@brocker.in>
2024-03-03 09:05:53 -0800

    increment build string and replace firmware binary

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -60,7 +60,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Brandon Brocker, 20240229, config: Brandon's BTT_GTR_V1_0)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Brandon Brocker, 20240303, config: Brandon's BTT_GTR_V1_0)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
