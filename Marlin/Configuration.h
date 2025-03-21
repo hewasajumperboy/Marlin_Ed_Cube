46cc0902fdd94ab06310e70c40ade5e2b089899a
hewasajumperboy <brandon@brocker.in>
2025-03-21 10:00:14 -0700

    Bump Z steps/mm to reflect mechanical connection (old value loses 0.1mm/20mm)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1197,7 +1197,7 @@
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 500 }
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 398, 94.4 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 94.4 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
