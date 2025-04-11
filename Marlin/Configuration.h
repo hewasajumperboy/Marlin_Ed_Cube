467eb4ee18341fdffb20a74ba911f606fc5ba28b
hewasajumperboy <brandon@brocker.in>
2025-04-11 13:04:59 -0700

    bump config string date

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -61,7 +61,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "Brandon Brocker, 20250410, MPX.3" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "Brandon Brocker, 20250411, MPX.3" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
