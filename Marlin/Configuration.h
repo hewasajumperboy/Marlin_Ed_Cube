1493077527698ff389286ef229344d2b4fe3989b
hewasajumperboy <brandon@brocker.in>
2025-07-06 09:05:14 -0700

    Add calibrated E-steps/mm to default config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1300,7 +1300,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 99.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 99.90 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
